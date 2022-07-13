db97e09d38e18977c27c1993b7b2fc4b6ac71d3a
Nkqubela Jojo <jnkqubela@gmail.com>
2022-07-13 14:49:39 +0200

    fixing it about icons

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
 
 function Announce() {
     return (
         <div>
             <h2>Hurry up it's 40% off now</h2>
+            <CloseIcon/>
         </div>
-    )
+    );
 }
 
 export default Announce;
