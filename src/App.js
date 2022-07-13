eb43b3430be76203375188274db4c409ba3ac5a3
Nkqubela Jojo <jnkqubela@gmail.com>
2022-07-13 13:52:37 +0200

    added home.jsx write

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="mt-5 ml-5">Hey I am a react app here</h1>
+      <Home/>
     </div>
   );
 }
