b6f6e9b3ce31982008234f6c79afeefcbb20feb7
Nkqubela Jojo <jnkqubela@gmail.com>
2022-07-13 15:42:14 +0200

    added onclick hidden

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,11 +1,16 @@
 import CloseIcon from '@mui/icons-material/Close';
-import React from "react";
+import {React, useState} from "react";
 
 function Announce() {
+    const [announceStyle, setAnnounceStyle] = useState("bg-[#8a4af3] font-bold text-white flex items-center justify-center")
+
+    const handleClose = ()=> {
+        setAnnounceStyle(announceStyle + " hidden")
+    }
     return (
-        <div className="bg-[#8a4af3] font-bold text-white flex items-center justify-center">
+        <div className= {announceStyle}>
             <h2>Hurry up it's 40% off now</h2>
-            <CloseIcon className="cursor-pointer" />
+            <CloseIcon className="cursor-pointer" onClick={handleClose}/>
         </div>
     );
 }
