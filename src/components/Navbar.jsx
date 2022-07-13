91e4e7380c32f82e4a705ec78f889a79f46f60b5
Nkqubela Jojo <jnkqubela@gmail.com>
2022-07-13 16:08:59 +0200

    added search input navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
+import { Search } from '@mui/icons-material';
 import React from 'react'
 
 const Navbar = () => {
     return (
         <div className="navbar h-[60px] shadow-md relative z-10">
             <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb[10px] flex justify-between items-center">
-                <div>1</div>
+                {/* left div */}
+                <div className='left flex flex-1 items-center'>
+                    <div className='language cursor-pointer text-[16px]'>En</div>
+
+                    <div className='searchInput flex border-[2px] border-solid border-lightgrey rounded-md items-center ml-[10px] p-[5px] focus-within:border-[#8a4af3] transition-all'>
+                        <input className='input outline-none' type="text" />
+                        <Search className='' style={{fontSize: '16px'}}/>
+                    </div>
+                </div> 
+
+
+
+
+
+
                 <div>2</div>
                 <div>3</div>
             </div>
